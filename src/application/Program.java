b91252ca23dcf58bd7f819dd42e469f97f8122e8
Roberto Bruno <robertobcb@gmail.com>
2022-07-22 18:05:50 -0300

    Solução ruim de Exception

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,6 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Date now = new Date();
 
 		System.out.print("Room number: ");
 		int number = sc.nextInt();
@@ -39,23 +38,15 @@ public class Program {
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-out date (dd//MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
-			reservation.updateDates(checkIn, checkOut);
-
-			if (checkIn.before(now) || checkOut.before(now)) {
-
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-
-			} else if (!checkOut.after(checkIn)) {
-
-				System.out.println("Error in reservation: Check-out date must be after check-in date");
-
-			} else {
-
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error != null) {
+				System.out.println("Error in Reservation: " + error);
+			}
+			else {
 				reservation = new Reservation(number, checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
-
 			}
-
+			
 		}
 
 	}
